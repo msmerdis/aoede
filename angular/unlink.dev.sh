11eed4c5a148c04ecd5af1de612e4af3c6eeaec1
Miltiadis Smerdis <msmerdis@gmail.com>
2022-07-23 19:20:18 +0300

    fix unlink script
     - remove symlinks created in node modules

--- angular/unlink.dev.sh
+++ angular/unlink.dev.sh
@@ -4,5 +4,19 @@ for d in *; do
 	if [ -d $dep ]; then
 		rm -rf $dep
 	fi
+
+	dist="$d/dist"
+	if [ -d $dist ]; then
+		rm -rf $dist
+	fi
+
+	for f in *; do
+		if [ -d $d ] && [ -d $f ]; then
+			node="$d/node_modules/$f"
+			if [ -L $node ]; then
+				rm $node
+			fi
+		fi
+	done
 done
 popd > /dev/null
